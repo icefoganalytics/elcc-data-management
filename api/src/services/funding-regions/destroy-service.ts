9dc8eb2901a2db9b9339e9aaa72d9ab1a4f5bdc9
Marlen Brunner <klondikemarlen@gmail.com>
2026-04-17 10:17:55 -0700

    :unlock: Add ability to destroy funding regions with building expense categories.
    Why? As a rule and recently created entity should be destroyable as long as it hasn't been used by the user.

--- api/src/services/funding-regions/destroy-service.ts
+++ api/src/services/funding-regions/destroy-service.ts
@@ -1,35 +1,27 @@
-import { BuildingExpenseCategory, Centre, FundingRegion, User } from "@/models"
+import db, { BuildingExpenseCategory, Centre, FundingRegion, User } from "@/models"
+import { BuildingExpenseCategories } from "@/services"
 import BaseService from "@/services/base-service"
 
 export class DestroyService extends BaseService {
   constructor(
     private fundingRegion: FundingRegion,
-    private _currentUser: User
+    private currentUser: User
   ) {
     super()
   }
 
   async perform(): Promise<void> {
-    await this.assertNoDependentEntitiesExist(this.fundingRegion.id)
+    await db.transaction(async () => {
+      await this.assertNoDependentEntitiesExist(this.fundingRegion.id)
 
-    await this.fundingRegion.destroy()
+      await this.removeDependentEntities(this.fundingRegion.id)
+
+      await this.fundingRegion.destroy()
+    })
   }
 
   private async assertNoDependentEntitiesExist(fundingRegionId: number) {
-    await this.assertNoDependentBuildingExpenseCategoriesExist(fundingRegionId)
     await this.assertNoDependentCentresExist(fundingRegionId)
-  }
-
-  private async assertNoDependentBuildingExpenseCategoriesExist(fundingRegionId: number) {
-    const buildingExpenseCategoryCount = await BuildingExpenseCategory.count({
-      where: {
-        fundingRegionId,
-      },
-    })
-
-    if (buildingExpenseCategoryCount > 0) {
-      throw new Error("Funding region with building expense categories cannot be deleted")
-    }
   }
 
   private async assertNoDependentCentresExist(fundingRegionId: number) {
@@ -42,6 +34,26 @@ export class DestroyService extends BaseService {
     if (centresCount > 0) {
       throw new Error("Funding region with centres cannot be deleted")
     }
+  }
+
+  private async removeDependentEntities(fundingRegionId: number) {
+    await this.removeDependentBuildingExpenseCategories(fundingRegionId)
+  }
+
+  private async removeDependentBuildingExpenseCategories(fundingRegionId: number) {
+    await BuildingExpenseCategory.findEach(
+      {
+        where: {
+          fundingRegionId,
+        },
+      },
+      async (buildingExpenseCategory) => {
+        await BuildingExpenseCategories.DestroyService.perform(
+          buildingExpenseCategory,
+          this.currentUser
+        )
+      }
+    )
   }
 }
 
